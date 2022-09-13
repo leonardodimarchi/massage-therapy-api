0c2f006ab94450083431fee362da3aded14b2f7e
User <leonardodimarchi@outlook.com>
2022-09-13 19:59:33 -0300

    feature(usecases/appointment): adding start date validation

--- src/domain/usecases/appointment/create_appointment_usecase.ts
+++ src/domain/usecases/appointment/create_appointment_usecase.ts
@@ -1,5 +1,6 @@
 import { AppointmentRepository } from "@/domain/contracts/repositories/appointment_repository";
 import { AppointmentEntity } from "@/domain/entities/appointment_entity";
+import { ValidationException } from "@/domain/exceptions/validation_exception";
 import { AppointmentPayload } from "@/domain/models/payloads/appointment_payload";
 
 export class CreateAppointmentUsecase implements UseCase<AppointmentEntity, AppointmentPayload> {
@@ -8,7 +9,10 @@ export class CreateAppointmentUsecase implements UseCase<AppointmentEntity, Appo
         private readonly repository: AppointmentRepository,
     ) {}
 
-    public async call(params: AppointmentPayload): Promise<AppointmentEntity> { 
+    public async call(params: AppointmentPayload): Promise<AppointmentEntity> {
+        if (params.startsAt.getTime() < new Date().getTime())
+            throw new ValidationException('A data de agendamento não pode ser antes da data de hoje');
+        
         return await this.repository.create(params);
     }
 }
--- test/domain/usecases/appointment/create_appointment_usecase.test.ts
+++ test/domain/usecases/appointment/create_appointment_usecase.test.ts
@@ -1,5 +1,6 @@
 import { AppointmentRepository } from "@/domain/contracts/repositories/appointment_repository";
 import { AppointmentEntity } from "@/domain/entities/appointment_entity";
+import { ValidationException } from "@/domain/exceptions/validation_exception";
 import { AppointmentPayload } from "@/domain/models/payloads/appointment_payload";
 import { CreateAppointmentUsecase } from "@/domain/usecases/appointment/create_appointment_usecase";
 import { MockProxy, mock } from "jest-mock-extended";
@@ -33,7 +34,7 @@ describe('CreateAppointmentUsecase', () => {
         startsAt: new Date(2023, 7, 20),
         endsAt: new Date(2023, 8, 4)
     });
-    
+
     it('should get a user when calling the repository successfully', async () => {
         repository.create.mockResolvedValue(entity);
 
@@ -41,5 +42,24 @@ describe('CreateAppointmentUsecase', () => {
 
         expect(result).toBe(entity);
         expect(repository.create).toHaveBeenNthCalledWith(1, payload);
+    });
+
+    it('should not create if the start date is before now', async () => {
+        const todayDate = new Date(2022, 10, 2);
+        const startsAt = new Date(2022, 10, 1);
+        const endsAt = new Date(2022, 10, 2);
+
+        jest.useFakeTimers().setSystemTime(todayDate);
+
+        const invalidPayload = new AppointmentPayload({
+            ...payload,
+            startsAt,
+            endsAt,
+        })
+
+        expect(async () => {
+            await usecase.call(invalidPayload)
+        }).rejects.toThrowError(ValidationException);
+        expect(repository.create).not.toHaveBeenCalled();
     });
 });
