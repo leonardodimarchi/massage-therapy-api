3e70954d6bfe45068e8a8c2ffedebf3c300bd5e8
User <leonardodimarchi@outlook.com>
2022-09-20 19:15:20 -0300

    feature(main): setting up swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './presenter/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Massage Therapy')
+    .setDescription('The API for the massage therapy app')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  
   await app.listen(3000);
 }
+
 bootstrap();
