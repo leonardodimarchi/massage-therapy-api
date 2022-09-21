5863a90c3752da4424e05d29e22ef0c70fd7e36a
User <leonardodimarchi@outlook.com>
2022-09-20 22:07:50 -0300

    feature(presenter/dto && presenter/controller): added dto and description at the login endpoint

--- src/presenter/controllers/auth_controller.ts
+++ src/presenter/controllers/auth_controller.ts
@@ -1,9 +1,10 @@
 import { UserEntity } from "@/domain/entities/user_entity";
-import { JwtProxy } from "@/domain/models/proxies/jwt_proxy";
 import { LoginUsecase } from "@/domain/usecases/user/login_usecase";
 import { LocalAuthGuard } from "@/infra/guards/authentication/local_auth_guard";
 import { Controller, UseGuards, Post, Request } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiBody, ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { JwtDto } from "@/presenter/dto/auth/jwt.dto";
+import { LoginDto } from "../dto/auth/login.dto";
 
 @ApiTags('Authentication')
 @Controller('auth')
@@ -14,7 +15,18 @@ export class AuthController {
 
     @UseGuards(LocalAuthGuard)
     @Post('login')
-    public login(@Request() req: { user: UserEntity }): JwtProxy {
-      return this.loginUsecase.call(req.user);
+    @ApiBody({
+      description: 'Os dados para login',
+      type: LoginDto,
+      required: true,
+    })
+    @ApiOperation({ summary: 'Gerar um token de usuário (JWT)' })
+    @ApiOkResponse({ description: 'O token foi gerado com sucesso', type: JwtDto })
+    public login(@Request() req: { user: UserEntity }): JwtDto {
+      const jwt = this.loginUsecase.call(req.user);
+
+      return {
+        ...jwt
+      }
     }
 }
