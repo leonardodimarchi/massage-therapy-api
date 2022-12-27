94b7b38a2dcd40a959ab44205010aaba8f9d44aa
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2022-12-27 14:16:24 -0300

    feature(database && mappers): adding TypeOrm mappers and refactoring folders

---
+++ src/infrastructure/database/typeorm/mappers/typeorm-appointment.mapper.ts
@@ -0,0 +1,39 @@
+import { AppointmentEntity } from "@/domain/entities/appointment_entity";
+import { EntitySchema } from "typeorm";
+
+export class TypeOrmAppointmentMapper {
+    static toSchema(appointment: AppointmentEntity) {
+        return {
+            complaint: appointment.complaint,
+            createdAt: appointment.createdAt,
+            endsAt: appointment.endsAt,
+            id: appointment.id,
+            isPregnant: appointment.isPregnant,
+            isUnderMedicalTreatment: appointment.isUnderMedicalTreatment,
+            pregnantWeeks: appointment.pregnantWeeks,
+            startsAt: appointment.startsAt,
+            status: appointment.status,
+            symptoms: appointment.symptoms,
+            updatedAt: appointment.updatedAt,
+            userId: appointment.userId,
+        };
+    }
+
+    static toDomain(raw: any): AppointmentEntity {
+        return new AppointmentEntity({
+            complaint: raw.complaint,
+            endsAt: raw.endsAt,
+            isUnderMedicalTreatment: raw.isUnderMedicalTreatment,
+            startsAt: raw.startsAt,
+            status: raw.status,
+            symptoms: raw.symptoms,
+            userId: raw.userId,
+            isPregnant: raw.isPregnant,
+            pregnantWeeks: raw.pregnantWeeks,
+        }, {
+            id: raw.id,
+            createdAt: raw.createdAt,
+            updatedAt: raw.updatedAt,
+        })
+    }
+}
--- src/infrastructure/database/typeorm/scripts/generate-migration.ts
+++ src/infrastructure/database/typeorm/scripts/generate-migration.ts
@@ -1,6 +1,6 @@
 import { exec } from 'child_process';
 
-const command = `npm run typeorm:cli -- migration:generate ./src/infrastructure/database/migrations/${process.argv[2]}`;
+const command = `npm run typeorm:cli -- migration:generate ./src/infrastructure/database/typeorm/migrations/${process.argv[2]}`;
 
 (() => exec(command, (error, stdout, stderr) => {
   if (error !== null) {
